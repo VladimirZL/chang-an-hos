43f0724df79e8ddbe3c12b721397ab8cff49cc40
18392192892 <18392192892@163.com>
2020-03-05 16:18:01 +0800

    first

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.less';
+
+import Register from './pages/Register/index.jsx';
+
+render (
+	<div>
+		<Register />
+	</div>,
+	document.getElementById('root')
+)
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.less';
+
+class Register extends React.Component {
+	render () {
+		return (
+			<div>这里是注册页</div>
+		)
+	}
+}
+
+export default Register;
